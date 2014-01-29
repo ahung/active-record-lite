5aab2caac1a86883ed2206f972e8033f462bacf4
Albert Hung <ahung88@gmail.com>
2014-01-28 17:43:37 -0800

    Completed searchable

--- lib/active_record_lite/03_searchable.rb
+++ lib/active_record_lite/03_searchable.rb
@@ -4,9 +4,23 @@ require_relative '02_sql_object'
 module Searchable
   def where(params)
     # ...
+    where_param_arr = params.keys.map { |key| "#{key} = ?"}
+    where_param = where_param_arr.join(" AND ")
+    values = params.values
+    
+    results = DBConnection.execute(<<-SQL, *values)
+    SELECT
+      *
+    FROM
+      #{table_name}
+    WHERE
+      #{where_param}
+    SQL
+    
+    results.map { |result| self.new(result) }
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
